e678cb8c2d432b500fdb91d9aa589048b18a3dfe
Angel Garces <angelgarces1248@gmail.com>
2021-12-24 14:46:54 -0400

    antes de branch

--- src/ValidT/ValidT.hs
+++ src/ValidT/ValidT.hs
@@ -4,6 +4,7 @@ Description : Provides necessary tools for lexing.
 Maintainer  : 15-11139@usb.ve, 16-10400@usb.ve 17-10538@usb.ve
 Stability   : experimental
 Portability : POSIX
+Main version
 -}
 
 module ValidT.ValidT where
